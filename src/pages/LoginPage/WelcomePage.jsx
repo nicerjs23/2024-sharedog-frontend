96d762aba39a8c1a9d5689e60c51c5eee85f3c8f
nicerjs23 <nicerjs23@gmail.com>
2024-11-29 14:42:43 +0000

    배포

---
+++ src/pages/LoginPage/WelcomePage.jsx
@@ -0,0 +1,33 @@
+import * as S from "./WelcomePage.styled";
+import Logo from "@assets/icons/Logo.svg";
+import KakaoLogo from "@assets/icons/kakao.svg";
+import { useCustomNavigate } from "@hooks/useCustomNavigate";
+export const WelcomePage = () => {
+  const { goTo } = useCustomNavigate();
+  return (
+    <S.Wrapper>
+      <S.Header onClick={() => goTo("/main")}>둘러보기</S.Header>
+      <S.Title>
+        나눠주개
+        <S.TitleInfo>반려견 헌혈 및 수혈까지 편리하게!</S.TitleInfo>
+      </S.Title>
+      <S.LogoWrapper>
+        <S.Logo src={Logo} alt="나눠주개 로고" />
+      </S.LogoWrapper>
+
+      <S.BtnWrapper>
+        <S.Btn onClick={() => goTo("/main")} type="kakao">
+          <S.Kakao src={KakaoLogo} alt="카카오 로고" />
+          카카오로 로그인하기
+        </S.Btn>
+        <S.Btn onClick={() => goTo("/main")} type="email">
+          이메일로 로그인하기
+        </S.Btn>
+        <S.SignUp>
+          아직 회원이 아니신가요?
+          <S.SignUpLink to="/main">회원가입하기</S.SignUpLink>
+        </S.SignUp>
+      </S.BtnWrapper>
+    </S.Wrapper>
+  );
+};
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,57 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      // PWA 플러그인 설정
+      registerType: "autoUpdate", // 서비스 워커 자동 업데이트 설정
+      includeAssets: [
+        "favicon.ico",
+        "robots.txt",
+        "/icons/maskable_icon_x128.png",
+        "/icons/maskable_icon_x192.png",
+        "/icons/maskable_icon_x512.png",
+        "/icons/maskable_icon_x512_maskable.png",
+      ], // 추가적으로 참조할 파일 경로
+      manifest: {
+        name: "나눠주개",
+        short_name: "나눠주개",
+        description: "반려견 헌혈 및 수혈까지 편리하게!",
+        start_url: "/", // PWA가 시작되는 URL
+        scope: "/", // PWA가 적용될 URL 범위
+        display: "standalone", // 브라우저 요소를 제거하고 앱처럼 보이게 설정
+        background_color: "#ffffff", // 배경 색상 (스플래시 화면)
+        theme_color: "#000000", // 툴바와 같은 UI 요소의 테마 색상
+        icons: [
+          {
+            src: "icons/maskable_icon_x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "icons/maskable_icon_x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icons/maskable_icon_x512_maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable", // 마스킹이 가능한 아이콘
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       //경로 별칭(Alias)으로 경로 관리하기
@@ -14,6 +62,7 @@ export default defineConfig({
       "@layout": path.resolve(__dirname, "src/layout"),
       "@pages": path.resolve(__dirname, "src/pages"),
       "@styles": path.resolve(__dirname, "src/styles"),
+      "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
 });

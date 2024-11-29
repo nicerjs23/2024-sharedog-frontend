18ac22cea28665e0dcbf6681b7f93bfa3557de25
nicerjs23@gmail.com <nicerjs23@gmail.com>
2024-11-29 15:57:13 +0900

    Feat: 훅 추가 웰컴페이지 생성

---
+++ src/hooks/useCustomNavigate.js
@@ -0,0 +1,55 @@
+import { useNavigate } from "react-router-dom";
+
+export const useCustomNavigate = () => {
+  const navigate = useNavigate();
+
+  const goTo = (path) => {
+    navigate(path);
+  };
+  const goBack = () => {
+    navigate(-1); //뒤로가기, +1은 앞으로가기
+  };
+  const goToWithId = (path, id) => {
+    navigate(`${path}/${id}`);
+  };
+
+  return { goTo, goBack, goToWithId };
+};
+
+/* 훅 사용법! 참고하세요~
+import React from "react";
+import { useCustomNavigate } from "@hooks/useCustomNavigate";
+
+const ExampleComponent = () => {
+  const { goTo, goBack } = useCustomNavigate();
+
+  return (
+    <div>
+      <button onClick={() => goTo("/target-path")}>Go to Target Page</button>
+      <button onClick={goBack}>Go Back</button>
+    </div>
+  );
+};
+
+export default ExampleComponent;
+*/
+
+// // useCustomNavigate.js
+// import { useNavigate, useLocation } from "react-router-dom";
+
+// export const useCustomNavigate = () => {
+//   const navigate = useNavigate();
+//   const location = useLocation();
+
+//   const goBack = () => {
+//     // 현재 경로가 글쓰기 페이지일 경우 자유게시판으로 이동
+//     if (location.pathname === "/write") {
+//       navigate("/board/free", { replace: true });
+//     } else {
+//       // 그 외의 경우 일반 뒤로 가기
+//       navigate(-1);
+//     }
+//   };
+
+//   return { goBack };
+// };
---
+++ src/pages/LoginPage/WelcomePage.jsx
@@ -0,0 +1,5 @@
+import * as S from "./WelcomePage.styled";
+
+export const WelcomePage = () => {
+  return <S.Wrapper>나눠주개 메인페이지</S.Wrapper>;
+};

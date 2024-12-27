f7ed5d46aed6937394b2d4231b8d362e38b55e60
2021110765@dgu.ac.kr <2021110765@dgu.ac.kr>
2024-12-27 17:46:59 +0900

    [Feature] #19 - 로그인 UI 구현 완료

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { DefaultLayout } from "@layout/DefaultLayout";
 import { MainPage } from "@pages/mainPage/MainPage";
 import { WelcomePage } from "@pages/loginPage/WelcomePage";
+import { LoginPage } from "@pages/loginPage/LoginPage";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -9,6 +10,7 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <WelcomePage /> },
       { path: "main", element: <MainPage /> },
+      { path: "login", element: <LoginPage /> },
     ],
   },
 ]);

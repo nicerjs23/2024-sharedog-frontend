85186710ce394bd9263c0a06e8ddbe1d63448aee
nicerjs23@gmail.com <nicerjs23@gmail.com>
2024-12-30 18:46:53 +0900

    ♻️ [Refactor] #21 - 라우터 중첩설정변경

--- src/pages/signUpPage/SignUpPage.jsx
+++ src/pages/signUpPage/SignUpPage.jsx
@@ -7,16 +7,13 @@ export const SignUpPage = () => {
       <S.MainWrapper>
         <S.EmailWrapper>
           <S.Text>
-            견주님의 <br/> 이메일 주소를 알려주세요
-          </S.Text>  
+            견주님의 <br /> 이메일 주소를 알려주세요
+          </S.Text>
           <span>이메일</span>
-          <S.EmailPlace 
-            type="email"
-            placeholder="abcd@naver.com"  
-          />
+          <S.EmailPlace type="email" placeholder="abcd@naver.com" />
         </S.EmailWrapper>
         <Button />
-      </S.MainWrapper>;
+      </S.MainWrapper>
     </>
-  )
+  );
 };

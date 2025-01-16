09f2580995706963c52b77008396d7820cf012cf
pedro0527 <sooyong0527@naver.com>
2025-01-16 22:20:44 +0900

    feat : signup header navigate 설정

--- src/layout/SignUpLayout.jsx
+++ src/layout/SignUpLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import XButton from "@assets/icons/X.svg";
 import LeftButton from "@assets/icons/Left.svg";
@@ -13,11 +13,19 @@ export const SignUpLayout = () => {
 };
 
 const Header = () => {
+  const navigate = useNavigate();
+  const BackClick = () => {
+    navigate(-1);
+  }
+  const XClick = () => {
+    navigate("/");
+  }
+
   return (
     <>
       <HeaderWrapper>
-        <img src={LeftButton} alt="백 버튼" />
-        <img src={XButton} alt="X 버튼" />
+        <img src={LeftButton} alt="백 버튼" onClick={BackClick}/>
+        <img src={XButton} alt="X 버튼" onClick={XClick}/>
       </HeaderWrapper>
     </>
   );
@@ -34,4 +42,8 @@ const HeaderWrapper = styled.section`
   background: #FAFAFC;
   justify-content: space-between;
   margin-bottom: 16px;
+
+  img {
+    cursor: pointer;
+  }
 `;

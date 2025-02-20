8c4140cb1550860b375f11a42c8af1d7735bd903
pedro0527 <sooyong0527@naver.com>
2025-02-20 20:43:25 +0900

    Feat : 커뮤니티 검색 기능 구현(api 연결 필요)

--- src/pages/communityPage/CommunitySearch.jsx
+++ src/pages/communityPage/CommunitySearch.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./CommunitySearch.styled";
 import Left from "@assets/icons/Left.svg";
 import Search from "@assets/icons/Search.svg";
+import SearchX from "@assets/icons/SearchX.svg";
 
 export const CommunitySearch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleClearSearch = () => {
+    setSearchTerm("");
+  };
+
   return (
     <>
       <S.Wrapper>
@@ -16,7 +23,17 @@ export const CommunitySearch = () => {
               <S.SearchIcon>
                 <img src={Search} alt="검색 아이콘" />
               </S.SearchIcon>
-              <S.Text>검색어를 입력하세요.</S.Text>
+              <S.SearchInput
+                type="text"
+                placeholder="검색어를 입력하세요."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              {searchTerm && (
+                <S.ClearButton onClick={handleClearSearch}>
+                  <img src={SearchX} alt="검색어 삭제" />
+                </S.ClearButton>
+              )}
             </S.RightContainer>
           </S.HeaderRight>
         </S.Header>

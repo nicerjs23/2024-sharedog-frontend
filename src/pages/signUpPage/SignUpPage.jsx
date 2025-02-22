779e639661b662cb8f9150cf0162072f6f2d654a
pedro0527 <sooyong0527@naver.com>
2025-02-22 16:04:52 +0900

    Fix : 이메일 회원가입에서 test@nav.com 처럼 이상한 이메일이여도 회원가입 되는 오류 수정

--- src/pages/signUpPage/SignUpPage.jsx
+++ src/pages/signUpPage/SignUpPage.jsx
@@ -2,22 +2,42 @@ import React, { useState } from "react";
 import { SignUpForm } from "@components/signup/SignUpForm";
 import Button from "@components/common/CommonButton";
 
+const allowedDomains = [
+  "gmail.com",
+  "naver.com",
+  "daum.net",
+  "yahoo.com",
+  "hotmail.com",
+  "outlook.com",
+  "icloud.com",
+  "live.com",
+  "nate.com",
+  "hanmail.net"
+];
+
 const SignUpPage = ({ formData, updateFormData, nextStep }) => {
     const [showError, setShowError] = useState(false);
+
+    const isValidEmail = (email) => {
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if(!emailRegex.test(email)) return false;
+
+      const domain = email.split("@")[1];
+      return allowedDomains.includes(domain);
+  };
 
     const handleEmailChange = (e) => {
         const email = e.target.value;
         updateFormData("email", email);
 
-        // 이메일 유효성 검사
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (emailRegex.test(email)) {
-            setShowError(false);
+        if(isValidEmail(email)) {
+          setShowError(false);
         }
     };
 
     const handleNext = () => {
-        if (!formData.email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+        if (!isValidEmail(formData.email)) {
             setShowError(true);
             return;
         }
@@ -35,7 +55,7 @@ const SignUpPage = ({ formData, updateFormData, nextStep }) => {
             showError={showError}
             errorMessage="올바른 이메일 주소를 입력해 주세요."
         >
-            <Button type="button" onClick={handleNext} bgColor={formData.email ? "#FF6969" : "#BDBDBD"}>
+            <Button type="button" onClick={handleNext} bgColor={isValidEmail(formData.email) ? "#FF6969" : "#BDBDBD"}>
                 다음
             </Button>
         </SignUpForm>

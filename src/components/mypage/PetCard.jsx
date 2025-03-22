973b4bf0d8e4a0f43f8981800f41831ee2541c14
DongGun Lee <124961407+nicerjs23@users.noreply.github.com>
2025-03-23 03:55:54 +0900

    💄 Design#136 -  (#154)
    * Feat: 배포캐시 문제 새로고침하는 코드 추가
    
    * Fix: 반려견 사진깨지는거 추가시 글씨깨지는거 수정

--- src/components/mypage/PetCard.jsx
+++ src/components/mypage/PetCard.jsx
@@ -18,7 +18,7 @@ export const PetCard = ({
   const navigate = useNavigate(); // useNavigate 훅 초기화
 
   const handleEdit = () => {
-    navigate('/petedit', {
+    navigate('/mypage/petedit', {
       state: {
         id,
         represent,
@@ -39,39 +39,43 @@ export const PetCard = ({
         <S.CardContents>
           <S.NameSection>
             {represent && <img src={Medal} alt="대표강아지 아이콘" />}
-            {name}
+            <S.NameWrapper>{name}</S.NameWrapper>
             <S.ProfileEdit onClick={handleEdit}>
               <img src={Pencil} alt="연필 아이콘" />
               프로필 수정
             </S.ProfileEdit>
           </S.NameSection>
-          <S.TitleSection>
-            <S.TitleText>
-              나이:<S.DetailText> {age}</S.DetailText>
-            </S.TitleText>
-            <S.TitleText>
-              몸무게:<S.DetailText> {weight}kg</S.DetailText>
-            </S.TitleText>
-            <S.TitleText>
-              성별: <S.DetailText>{gender}</S.DetailText>
-            </S.TitleText>
-            <S.TitleText>
-              중성화:
-              <S.DetailText>
-                {' '}
-                {neutered ? '수술 진행' : '수술 미진행'}
-              </S.DetailText>
-            </S.TitleText>
-            <S.TitleText>
-              혈액형:<S.DetailText> {bloodType}</S.DetailText>
-            </S.TitleText>
-          </S.TitleSection>
-        </S.CardContents>
 
-        <S.ProfileImage
-          src={profileImage || PetProfile}
-          alt="강아지 프로필사진"
-        />
+          <S.TitleImgDiv>
+            <div>
+              <S.TitleSection>
+                <S.TitleText>
+                  나이:<S.DetailText> {age}</S.DetailText>
+                </S.TitleText>
+                <S.TitleText>
+                  몸무게:<S.DetailText> {weight}kg</S.DetailText>
+                </S.TitleText>
+                <S.TitleText>
+                  성별: <S.DetailText>{gender}</S.DetailText>
+                </S.TitleText>
+                <S.TitleText>
+                  중성화:
+                  <S.DetailText>
+                    {' '}
+                    {neutered ? '수술 진행' : '수술 미진행'}
+                  </S.DetailText>
+                </S.TitleText>
+                <S.TitleText>
+                  혈액형:<S.DetailText> {bloodType}</S.DetailText>
+                </S.TitleText>
+              </S.TitleSection>
+            </div>
+            <S.ProfileImage
+              src={profileImage || PetProfile}
+              alt="강아지 프로필사진"
+            />
+          </S.TitleImgDiv>
+        </S.CardContents>
       </S.Card>
     </S.Wrapper>
   );

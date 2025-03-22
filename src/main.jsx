84956546204b737dd0deef3ada0d482e25c2eb4c
DongGun Lee <124961407+nicerjs23@users.noreply.github.com>
2025-03-23 03:28:38 +0900

    ✨ [Feature] #136 - 배포캐시 문제 새로고침하는 코드 추가 (#152)

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { registerSW } from 'virtual:pwa-register';
 
-createRoot(document.getElementById("root")).render(
+// ✅ 새 버전이 감지되면 자동 새로고침
+const updateSW = registerSW({
+  onNeedRefresh() {
+    updateSW(true); // 새 버전 즉시 업데이트 + 페이지 리프레시
+  },
+  onOfflineReady() {
+    console.log('오프라인 모드 사용 준비 완료!');
+  },
+});
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>

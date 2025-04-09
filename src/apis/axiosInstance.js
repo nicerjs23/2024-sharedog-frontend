18380f95f21824f1b0253517dedae4cf0996c5e2
nicerjs23 <nicerjs23@gmail.com>
2025-04-09 17:30:46 +0000

    배포

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,17 +1,17 @@
-import axios from "axios";
-import { refreshAccessToken } from "@apis/refreshTokenUtil";
+import axios from 'axios';
+import { refreshAccessToken } from '@apis/refreshTokenUtil';
 
 // Axios 인스턴스 생성
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL, // API 기본 URL을 환경 변수에서 가져옴
-  withCredentials: true, // 쿠키를 포함한 요청 허용
+  //withCredentials: true, // 쿠키를 포함한 요청 허용
 });
 
 // 요청 인터셉터
 axiosInstance.interceptors.request.use(
   (config) => {
     // 로컬 스토리지에서 Access Token을 가져와 Authorization 헤더에 추가
-    const token = localStorage.getItem("access");
+    const token = localStorage.getItem('access');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -34,7 +34,7 @@ axiosInstance.interceptors.response.use(
         const newAccessToken = await refreshAccessToken();
 
         if (!newAccessToken) {
-          throw new Error("새로운 Access Token을 받지 못했습니다.");
+          throw new Error('새로운 Access Token을 받지 못했습니다.');
         }
 
         // 새로운 Access Token을 헤더에 추가
@@ -43,11 +43,11 @@ axiosInstance.interceptors.response.use(
         // 원래 요청을 다시 보냄
         return axiosInstance(originalRequest);
       } catch (refreshError) {
-        console.error("토큰 갱신 실패:", refreshError);
+        console.error('토큰 갱신 실패:', refreshError);
 
         // 토큰 갱신 실패 시, 로컬 스토리지에서 토큰 제거 및 로그아웃 처리
-        localStorage.removeItem("access");
-        localStorage.removeItem("refresh");
+        localStorage.removeItem('access');
+        localStorage.removeItem('refresh');
 
         return Promise.reject(refreshError);
       }
--- src/router.jsx
+++ src/router.jsx
@@ -1,21 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
-import { DefaultLayout } from "@layout/DefaultLayout";
-import { SignUpLayout } from "@layout/SignUpLayout";
+import { createBrowserRouter } from 'react-router-dom';
+import { DefaultLayout } from '@layout/DefaultLayout';
+import { SignUpLayout } from '@layout/SignUpLayout';
 
-import { MainPage } from "@pages/mainPage/MainPage";
-import { WelcomePage } from "@pages/loginPage/WelcomePage";
+import { MainPage } from '@pages/mainPage/MainPage';
+import { WelcomePage } from '@pages/loginPage/WelcomePage';
 // import { SignUpPage } from "@pages/signUpPage/SignUpPage";
-import SignupFormContainer from "@pages/signUpPage/SignupFormContainer";
-// import { LoginPage } from "@pages/loginPage/LoginPage";
-import { CommunityNew } from "@pages/communityPage/CommunityNew";
-import { CommunitySearch } from "@pages/communityPage/CommunitySearch";
-import { ChatPage } from "@pages/chatPage/ChatPage";
-import { MyPage } from "@pages/myPage/MyPage";
-import { AccountManagement } from "@pages/myPage/AccountManagement";
-import { PetInfo } from "@pages/myPage/PetInfo";
-import { PetEdit } from "@pages/myPage/PetEdit";
-import { PetAdd } from "@pages/myPage/PetAdd";
-import { Reservation } from "@pages/myPage/Reservation";
+import SignupFormContainer from '@pages/signUpPage/SignupFormContainer';
+import { LoginPage } from '@pages/loginPage/LoginPage';
+import { CommunityNew } from '@pages/communityPage/CommunityNew';
+import { CommunitySearch } from '@pages/communityPage/CommunitySearch';
+import { ChatPage } from '@pages/chatPage/ChatPage';
+import { MyPage } from '@pages/myPage/MyPage';
+import { AccountManagement } from '@pages/myPage/AccountManagement';
+import { PetInfo } from '@pages/myPage/PetInfo';
+import { PetEdit } from '@pages/myPage/PetEdit';
+import { PetAdd } from '@pages/myPage/PetAdd';
+import { Reservation } from '@pages/myPage/Reservation';
 // import { PwSignUpPage } from "@pages/signUpPage/PwSignUpPage";
 // import { NameSignUpPage } from "@pages/signUpPage/NameSignUpPage";
 // import { PhoneSignUpPage } from "@pages/signUpPage/PhoneSignUpPage";
@@ -26,56 +26,56 @@ import { Reservation } from "@pages/myPage/Reservation";
 // import { LastSignUpPage } from '@pages/signUpPage/LastSignUpPage';
 // import { OpSignUpPage } from '@pages/signUpPage/OpSignUpPage';
 // import { BloodSignUpPage } from '@pages/signUpPage/BloodSignUpPage';
-import { TestStartPage } from "@pages/testPage/TestStartPage";
-import { TestLayout } from "@layout/TestLayout";
-import { TestPage } from "@pages/testPage/TestPage";
-import { TestResultOkPage } from "@pages/testPage/TestResultOkPage";
-import { TestResultNoPage } from "@pages/testPage/TestResultNoPage";
-import { KakaoCallbackPage } from "@pages/loginPage/KakaoCallbackPage";
-import { CommunityWrite } from "@pages/communityPage/CommunityWrite";
-import { CommunityDetail } from "@pages/communityPage/CommunityDetail";
+import { TestStartPage } from '@pages/testPage/TestStartPage';
+import { TestLayout } from '@layout/TestLayout';
+import { TestPage } from '@pages/testPage/TestPage';
+import { TestResultOkPage } from '@pages/testPage/TestResultOkPage';
+import { TestResultNoPage } from '@pages/testPage/TestResultNoPage';
+import { KakaoCallbackPage } from '@pages/loginPage/KakaoCallbackPage';
+import { CommunityWrite } from '@pages/communityPage/CommunityWrite';
+import { CommunityDetail } from '@pages/communityPage/CommunityDetail';
 // import { PetSignUpLayout } from '@layout/PetSignUpLayout';
-import { ChatDetailPage } from "@pages/chatPage/ChatDetailPage";
-import { TermsPage } from "@pages/myPage/TermsPage";
-import { PrivacyPolicyPage } from "@pages/myPage/PrivacyPolicyPage";
-import { MyWrite } from "@pages/myPage/MyWrite";
-import { BloodRule } from "@pages/mainPage/BloodRule";
-import { BloodCaution } from "@pages/mainPage/BloodCaution";
-import { BloodBenefit } from "@pages/mainPage/BloodBenefit";
-import { CommunityPolicy } from "@pages/communityPage/CommunityPolicy";
+import { ChatDetailPage } from '@pages/chatPage/ChatDetailPage';
+import { TermsPage } from '@pages/myPage/TermsPage';
+import { PrivacyPolicyPage } from '@pages/myPage/PrivacyPolicyPage';
+import { MyWrite } from '@pages/myPage/MyWrite';
+import { BloodRule } from '@pages/mainPage/BloodRule';
+import { BloodCaution } from '@pages/mainPage/BloodCaution';
+import { BloodBenefit } from '@pages/mainPage/BloodBenefit';
+import { CommunityPolicy } from '@pages/communityPage/CommunityPolicy';
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <DefaultLayout />, //완전 기본 세팅 + 공통푸터관리
     children: [
-      { path: "", element: <WelcomePage /> },
-      { path: "main", element: <MainPage /> },
-      { path: "bloodRule", element: <BloodRule /> },
-      { path: "bloodCaution", element: <BloodCaution /> },
-      { path: "bloodBenefit", element: <BloodBenefit /> },
-      // { path: "login", element: <LoginPage /> },
-      { path: "kakao/callback", element: <KakaoCallbackPage /> },
-      { path: "chatList", element: <ChatPage /> },
-      { path: "chat/:id", element: <ChatDetailPage /> },
+      { path: '', element: <WelcomePage /> },
+      { path: 'main', element: <MainPage /> },
+      { path: 'bloodRule', element: <BloodRule /> },
+      { path: 'bloodCaution', element: <BloodCaution /> },
+      { path: 'bloodBenefit', element: <BloodBenefit /> },
+      { path: 'login', element: <LoginPage /> },
+      { path: 'kakao/callback', element: <KakaoCallbackPage /> },
+      { path: 'chatList', element: <ChatPage /> },
+      { path: 'chat/:id', element: <ChatDetailPage /> },
       {
-        path: "mypage",
+        path: 'mypage',
 
         children: [
           { index: true, element: <MyPage /> },
           {
-            path: "accountmanagement",
+            path: 'accountmanagement',
             element: <AccountManagement />,
           },
-          { path: "petinfo", element: <PetInfo /> },
-          { path: "petedit", element: <PetEdit /> },
-          { path: "petadd", element: <PetAdd /> },
-          { path: "mywrite", element: <MyWrite /> },
-          { path: "reservation", element: <Reservation /> },
-          { path: "terms", element: <TermsPage /> },
-          { path: "privacy", element: <PrivacyPolicyPage /> },
+          { path: 'petinfo', element: <PetInfo /> },
+          { path: 'petedit', element: <PetEdit /> },
+          { path: 'petadd', element: <PetAdd /> },
+          { path: 'mywrite', element: <MyWrite /> },
+          { path: 'reservation', element: <Reservation /> },
+          { path: 'terms', element: <TermsPage /> },
+          { path: 'privacy', element: <PrivacyPolicyPage /> },
         ],
       },
-      { path: "testStart", element: <TestStartPage /> },
+      { path: 'testStart', element: <TestStartPage /> },
 
       // {
       //   path: 'signup',
@@ -100,23 +100,23 @@ export const router = createBrowserRouter([
   },
   //헌혈견테스트쪽은 아예따로 뺐음 테스트 시작화면만 디폴트레이아웃에 속하도록함
   {
-    path: "community",
+    path: 'community',
     element: <DefaultLayout />,
     children: [
-      { path: "", element: <CommunityNew /> },
-      { path: "search", element: <CommunitySearch /> },
-      { path: "write", element: <CommunityWrite /> },
-      { path: ":id", element: <CommunityDetail /> },
-      { path: "policy", element: <CommunityPolicy /> },
+      { path: '', element: <CommunityNew /> },
+      { path: 'search', element: <CommunitySearch /> },
+      { path: 'write', element: <CommunityWrite /> },
+      { path: ':id', element: <CommunityDetail /> },
+      { path: 'policy', element: <CommunityPolicy /> },
     ],
   },
   {
-    path: "/test",
+    path: '/test',
     element: <TestLayout />,
     children: [
-      { path: "", element: <TestPage /> },
-      { path: "resultOK", element: <TestResultOkPage /> },
-      { path: "resultNO", element: <TestResultNoPage /> },
+      { path: '', element: <TestPage /> },
+      { path: 'resultOK', element: <TestResultOkPage /> },
+      { path: 'resultNO', element: <TestResultNoPage /> },
     ],
   },
 ]);
